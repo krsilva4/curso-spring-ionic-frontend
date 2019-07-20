326bfa9bfb93489da90f5027851364af1d3fb4cc
kenne roberto <krsilva4@hotmail.com>
2019-07-20 12:56:49 -0300

    teste no heroku

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,5 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8081",
+    // baseUrl: "http://localhost:8081",
+    baseUrl: "https://curso-spring-ionic-pe-recife.herokuapp.com",
     bucketBaseUrl: "https://curso-spring-ionic-pe-recife.s3-sa-east-1.amazonaws.com"
 }  
--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
+import { ProdutoService } from '../../services/domain/produto.service';
 
 /**
  * Generated class for the ProdutosPage page.
@@ -16,23 +17,19 @@ import { ProdutoDTO } from '../../models/produto.dto';
 })
 export class ProdutosPage {
   items: ProdutoDTO[];
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public produtoService: ProdutoService,
+    public loadingCtrl: LoadingController) {
   }
 
   ionViewDidLoad() {
-    this.items = [
-      { 
-        id: "1",
-        nome: 'Mouse',
-        preco: 80.99     
-    },
-    { 
-      id: "2",
-      nome: 'teclado',
-      preco: 100.00     
-  }
-
-    ]
+    let categoria_id = this.navParams.get('categoria_id');
+    this.produtoService.findByCategoria(categoria_id)
+      .subscribe(response => {
+        this.items = response['content'];
+      },
+        error => { })
   }
 
 }
