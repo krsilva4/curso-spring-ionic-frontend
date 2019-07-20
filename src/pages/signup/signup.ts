7573ba80ac848eab5dc4ece438f4043706c673aa
kenne roberto <krsilva4@hotmail.com>
2019-07-20 11:42:31 -0300

    132. Salvando novo cliente

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -24,36 +24,40 @@ export class SignupPage {
   estados: EstadoDTO[];
   cidades: CidadeDTO[];
   formGroup: FormGroup;
-  constructor(public navCtrl: NavController, 
-              public navParams: NavParams,
-              public formBuilder: FormBuilder,
-              public cidadeService: CidadeService,
-              public estadoService: EstadoService,
-              public clienteService: ClienteService,
-              public alertCtrl: AlertController
-              ) {
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public formBuilder: FormBuilder,
+    public cidadeService: CidadeService,
+    public estadoService: EstadoService,
+    public clienteService: ClienteService,
+    public alertCtrl: AlertController
+  ) {
 
-                this.formGroup = this.formBuilder.group({
-                  nome: ['Joaquim', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
-                  email: ['joaquim@gmail.com', [Validators.required, Validators.email]],
-                  tipo : ['1', [Validators.required]],
-                  cpfOuCnpj : ['06134596280', [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
-                  senha : ['123', [Validators.required]],
-                  logradouro : ['Rua Via', [Validators.required]],
-                  numero : ['25', [Validators.required]],
-                  complemento : ['Apto 3', []],
-                  bairro : ['Copacabana', []],
-                  cep : ['10828333', [Validators.required]],
-                  telefone1 : ['977261827', [Validators.required]],
-                  telefone2 : ['', []],
-                  telefone3 : ['', []],
-                  estadoId : [null, [Validators.required]],
-                  cidadeId : [null, [Validators.required]]      
-                });
+    this.formGroup = this.formBuilder.group({
+      nome: ['Joaquim', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
+      email: ['joaquim@gmail.com', [Validators.required, Validators.email]],
+      tipo: ['1', [Validators.required]],
+      cpfOuCnpj: ['06134596280', [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
+      senha: ['123', [Validators.required]],
+      logradouro: ['Rua Via', [Validators.required]],
+      numero: ['25', [Validators.required]],
+      complemento: ['Apto 3', []],
+      bairro: ['Copacabana', []],
+      cep: ['10828333', [Validators.required]],
+      telefone1: ['977261827', [Validators.required]],
+      telefone2: ['', []],
+      telefone3: ['', []],
+      estadoId: [null, [Validators.required]],
+      cidadeId: [null, [Validators.required]]
+    });
   }
 
-  signupUser(){
-    console.log("Enviou o formulario");
+  signupUser() {
+    this.clienteService.insert(this.formGroup.value)
+      .subscribe(response => {
+        this.showInsertOk();
+      },
+        error => { });
   }
   ionViewDidLoad() {
     this.estadoService.findAll()
@@ -62,7 +66,7 @@ export class SignupPage {
         this.formGroup.controls.estadoId.setValue(this.estados[0].id);
         this.updateCidades();
       },
-      error => {});
+        error => { });
   }
   updateCidades() {
     let estado_id = this.formGroup.value.estadoId;
@@ -71,6 +75,23 @@ export class SignupPage {
         this.cidades = response;
         this.formGroup.controls.cidadeId.setValue(null);
       },
-      error => {});
+        error => { });
+  }
+
+  showInsertOk() {
+    let alert = this.alertCtrl.create({
+      title: 'Sucesso!',
+      message: 'Cadastro efetuado com sucesso',
+      enableBackdropDismiss: false,
+      buttons: [
+        {
+          text: 'Ok',
+          handler: () => {
+            this.navCtrl.pop();
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 }
