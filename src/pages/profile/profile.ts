62036b7c4b03e22eb6b017e99fd042c86720664b
kenne roberto <krsilva4@hotmail.com>
2019-07-24 13:23:21 -0300

    145. Armazenamento dos dados do pedido

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -34,7 +34,7 @@ export class ProfilePage {
     if (localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email)
         .subscribe(response => {
-          this.cliente = response;
+          this.cliente = response as ClienteDTO;
           // this.urlTest = API_CONFIG.bucketBaseUrl+'cp'+this.cliente.id+'.jpg';
           this.getEscolherImagem();
         },
