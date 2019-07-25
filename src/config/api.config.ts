708389c1d53346aa421be2214b8f196de76362eb
kenne roberto <krsilva4@hotmail.com>
2019-07-25 16:47:40 -0300

    teste heroku

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    baseUrl: "http://192.168.0.13:8081",
- //   baseUrl: "http://curso-spring-ionic-pe-recife.herokuapp.com", 
+ //   baseUrl: "http://192.168.0.13:8081",
+    baseUrl: "https://curso-spring-ionic-pe-re.herokuapp.com", 
     bucketBaseUrl: "https://curso-spring-ionic-pe-recife.s3-sa-east-1.amazonaws.com"
 }  
